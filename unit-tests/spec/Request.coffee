67ed2d6cb97114dc9e630e966b80742f77e742ec
Will Munn <willmunn@hotmail.com>
2011-11-23 15:59:45 +0000

    first coffee script test that was hard

---
+++ unit-tests/spec/Request.coffee
@@ -0,0 +1,3 @@
+describe "Request", ->
+  it "should be true", ->
+    expect(1).toEqual 1
---
+++ unit-tests/spec/Request.js
@@ -0,0 +1,6 @@
+
+  describe("Request", function() {
+    return it("should be true", function() {
+      return expect(1).toEqual(1);
+    });
+  });
